9ceb0c74f27c068f0920aa53f125d6f47afe09ca
Alejandro Lancellotti <alejandrov.lancellotti@gmail.com>
2023-03-09 02:34:12 -0300

    test 1

---
+++ .platform/hooks/prebuild/yarn.sh
@@ -0,0 +1,8 @@
+.platform/hooks/prebuild/yarn.sh
+# add yarn
+sudo wget https://dl.yarnpkg.com/rpm/yarn.repo -O /etc/yum.repos.d/yarn.repo 
+#install yarn
+sudo yum -y install yarn
+# install dependencies
+cd /var/app/staging/
+yarn
