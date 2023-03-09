f57b775bd78098b0d4411730d4866bc122f76a7d
Alejandro Lancellotti <alejandrov.lancellotti@gmail.com>
2023-03-09 04:52:06 -0300

    test 2

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { PokemonModule } from './pokemon/pokemon.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
-import { EnvConfiguration } from 'src/config/env.config';
+import { EnvConfiguration } from 'src/config/app.config';
 
 @Module({
   imports: [
